ea8fe2aed9615ef3653177cb0e6bc5c892ad2840
Elan Baharie <0elan1@gmail.com>
2017-07-13 17:52:19 -0700

    testing began

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "the add process for a product" do
+  it "will visit the correct page" do
+    visit products_path
+    expect(page).to have_content 'see'
+  end
+
+  it "will login an admin" do
+    user = User.create(:email => "superman@super.com", :password => "password")
+    user.admin = true
+    visit users_path
+    click_link "Login"
+    fill_in 'Email', :with => "superman@super.com"
+    fill_in 'Password', :with => "password"
+    click_button 'Log in'
+    expect(user.admin).to eq true
+  end
+
+  it "will add a product" do
+    user = User.create(:email => "superman@super.com", :password => "password")
+    user.admin = true
+    visit users_path
+    click_link "Login"
+    fill_in 'Email', :with => "superman@super.com"
+    fill_in 'Password', :with => "password"
+    click_button 'Log in'
+    visit new_product_path
+    fill_in 'Name', :with => "Lobster"
+    fill_in 'Description', :with => "Maine caught lobis mmmmm"
+    fill_in 'Cost', :with => "20.00"
+    fill_in 'Country', :with => "United States of America"
+    click_on 'Create Product'
+    expect(page).to have_content 'Lobster'
+  end
+
+
+
+
+end
